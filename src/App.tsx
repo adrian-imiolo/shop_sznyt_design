3d8b04b660f3b0b4e3f4245e04421ac71dab5e76
Adrian Imioło <adrian.imiolo11@gmail.com>
2026-03-17 19:02:46 +0100

    Refactor routing: extract ShopLayout with Outlet, isolate admin routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -9,22 +7,27 @@ import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 import { CartProvider } from "./context/CartContext";
 import ScrollOnNav from "./utils/ScrollOnNav";
+import AdminProducts from "./pages/admin/AdminProducts";
+import ShopLayout from "./components/ShopLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <CartProvider>
         <ScrollOnNav />
-        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sklep" element={<Shop />} />
-          <Route path="/sklep/:id" element={<ProductDetail />} />
-          <Route path="/o-nas" element={<About />} />
-          <Route path="/kontakt" element={<Contact />} />
-          <Route path="/koszyk" element={<Cart />} />
+          <Route element={<ShopLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/sklep" element={<Shop />} />
+            <Route path="/sklep/:id" element={<ProductDetail />} />
+            <Route path="/o-nas" element={<About />} />
+            <Route path="/kontakt" element={<Contact />} />
+            <Route path="/koszyk" element={<Cart />} />
+          </Route>
+          <Route path="/admin" element={<AdminProducts />} />
+          <Route path="/admin/produkty/nowy" element={<AdminProducts />} />
+          <Route path="/admin/produkty/:id" element={<AdminProducts />} />
         </Routes>
-        <Footer />
       </CartProvider>
     </BrowserRouter>
   );
